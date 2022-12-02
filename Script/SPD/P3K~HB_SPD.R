7cc8a8e14cedba1d0f51d0cc4f9fcbfd74a474a1
chantore <wnrnrxla@gmail.com>
2022-12-02 16:32:30 +0900

    Migration : 20221202

--- Script/SPD/P3K~HB_SPD.R
+++ Script/SPD/P3K~HB_SPD.R
@@ -4,6 +4,7 @@ library(rcarbon)
 
 # Data Load
 data <- read.csv("/Users/jch/Documents/github/P3K-HB_Demography/Data/Korea_CR_14C.csv")
+data <- na.omit(data)
 
 # Data Processing
 caldates <- calibrate(x=data$BP, errors=data$Error, calCurves="intcal20", normalised=FALSE, verbose=FALSE)
@@ -17,8 +18,8 @@ spd.rol <- spd(x=caldates, bins=bins, timeRange=c(2200, 1200), runm=50)
 
 #Plot
 tiff(file="/Users/jch/Documents/github/P3K-HB_Demography/Graph/Demographics(Observed_SPD).png", units='in', res=300, width=7, height=5)
-plot(spd.rol, calendar = "BP")
-plot(spd.raw, add=TRUE, calendar="BP", type="simple", col="black", lwd=1, lty=2)
+plot(spd.rol, calendar = "BCAD")
+plot(spd.raw, add=TRUE, calendar="BCAD", type="simple", col="black", lwd=1, lty=2)
 title("SPD(P3K~HB)")
 legend("topleft",legend=c("Observed SPD", "Rolling-Mean : 50"),col=c(1,"grey"),lty=c(2,1),lwd=c(1,5),cex=0.8,bg="white")
 dev.off()
